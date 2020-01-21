07bd33f9411c6c00038a18ff5987a8012f80cba0
kylejvh <49110126+kylejvh@users.noreply.github.com>
2020-01-21 00:35:07 -0500

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// ! Below is for a new nav implementation...
+
+// const navButton = document.querySelector("#nav-icon");
+// const navContainer = document.getElementById("navgroup");
+
+// window.onload = () => {
+//   navContainer.classList.toggle("navgroup-expand");
+// };
+
+// const toggleMenu = () => {
+//   if (navContainer.classList.contains("navgroup-expand")) {
+//     navContainer.classList.remove("navgroup-expand");
+//   } else {
+//     navContainer.classList.add("navgroup-expand");
+//   }
+// };
+
